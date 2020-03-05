eb54a3d3c687669b339d018977cd7a4a39e90b3c
NemanjaMilenkovic <nemanja.milenkovic@gmail.com>
2020-03-05 16:21:07 +0900

    port to TypeScript

--- App.tsx
+++ App.tsx
@@ -42,8 +42,8 @@ const App: () => React$Node = () => {
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
+                !Edit <Text style={styles.highlight}>App.js</Text> to change
+                this screen and then come back to see your edits.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
