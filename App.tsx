5c543892a9d00d43e5652023d3620ca2778230f9
KotaAoyama <hongcha811@gmail.com>
2020-03-17 01:08:01 +0900

    changed useEffect

--- App.tsx
+++ App.tsx
@@ -90,31 +90,32 @@ const App: () => React$Node = () => {
       if (payload.length !== 0) {
         stateA.setUserCurrentChallenge(payload[0]);
         stateA.setUserHasActiveChallenge(true);
-
-        const today = new Date();
-        const monthOfToday = today.getMonth() + 1;
-        const dateOfToday = today.getDate();
-        // console.log('montheOfToday, dateOfToday ************: ', monthOfToday, dateOfToday);
-        for (let [key, value] of Object.entries(stateA.userCurrentChallenge)) {
-        // for (const key in stateA.userCurrentChallenge) {
-          if (key.slice(0, 4) === "task" && key.slice(-4) === "Date") {
-            const targetDate = new Date(value);
-            // console.log('targetDate.getMonth() + 1, targetDate.getDate() ****************: ', targetDate.getMonth() + 1, targetDate.getDate());
-            if (monthOfToday === targetDate.getMonth() + 1 && dateOfToday === targetDate.getDate()) {
-              // console.log('aaaaaaaaa');
-              const currentDateStr = key.substring(key.indexOf('k') + 1, key.indexOf('D'));
-              stateA.setCurrentChallengeTodayDate(currentDateStr);
-              stateA.setCurrentChallengeTodayTaskName(stateA.userCurrentChallenge[`task${currentDateStr}Name`]);
-              stateA.setCurrentChallengeTodayTaskIsDone(stateA.userCurrentChallenge[`task${currentDateStr}IsDone`]);
-              break;
-            }
-          }
-        }
       }
     }).catch((error) => {
       console.log(error);
     });
   }, [state.user])
+
+  useEffect(() => {
+    const today = new Date();
+    const monthOfToday = today.getMonth() + 1;
+    const dateOfToday = today.getDate();
+    console.log('montheOfToday, dateOfToday ************: ', monthOfToday, dateOfToday);
+    for (let [key, value] of Object.entries(stateA.userCurrentChallenge)) {
+      if (key.slice(0, 4) === "task" && key.slice(-4) === "Date") {
+        const targetDate = new Date(value);
+        console.log('targetDate.getMonth() + 1, targetDate.getDate() ****************: ', targetDate.getMonth() + 1, targetDate.getDate());
+        if (monthOfToday === targetDate.getMonth() + 1 && dateOfToday === targetDate.getDate()) {
+          console.log('aaaaaaaaa');
+          const currentDateStr = key.substring(key.indexOf('k') + 1, key.indexOf('D'));
+          stateA.setCurrentChallengeTodayDate(currentDateStr);
+          stateA.setCurrentChallengeTodayTaskName(stateA.userCurrentChallenge[`task${currentDateStr}Name`]);
+          stateA.setCurrentChallengeTodayTaskIsDone(stateA.userCurrentChallenge[`task${currentDateStr}IsDone`]);
+          break;
+        }
+      }
+    }
+  }, [stateA.userCurrentChallenge])
 
   // console.log("stateA updated", stateA.userHasActiveChallenge ? "1" : "0");
 
