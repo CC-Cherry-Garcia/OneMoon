6ba7bb6073cda1e0c701ded59c23c6ad831b814c
KotaAoyama <hongcha811@gmail.com>
2020-03-16 15:30:02 +0900

    merge conflict

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -247,83 +247,6 @@ export const listChallenges = /* GraphQL */ `
     }
   }
 `;
-<<<<<<< Updated upstream
-export const searchChallengeByUser = /* GraphQL */ `
-  query searchChallengeByUser($userID: String!) {
-    listChallenges(filter: {userID: {eq: $userID}}) {
-      items {
-        id
-        userID
-        title
-        startDate
-        increase
-        isValid
-        task1Name
-        task1IsDone
-        task2Name
-        task2IsDone
-        task3Name
-        task3IsDone
-        task4Name
-        task4IsDone
-        task5Name
-        task5IsDone
-        task6Name
-        task6IsDone
-        task7Name
-        task7IsDone
-        task8Name
-        task8IsDone
-        task9Name
-        task9IsDone
-        task10Name
-        task10IsDone
-        task11Name
-        task11IsDone
-        task12Name
-        task12IsDone
-        task13Name
-        task13IsDone
-        task14Name
-        task14IsDone
-        task15Name
-        task15IsDone
-        task16Name
-        task16IsDone
-        task17Name
-        task17IsDone
-        task18Name
-        task18IsDone
-        task19Name
-        task19IsDone
-        task20Name
-        task20IsDone
-        task21Name
-        task21IsDone
-        task22Name
-        task22IsDone
-        task23Name
-        task23IsDone
-        task24Name
-        task24IsDone
-        task25Name
-        task25IsDone
-        task26Name
-        task26IsDone
-        task27Name
-        task27IsDone
-        task28Name
-        task28IsDone
-        task29Name
-        task29IsDone
-        task30Name
-        task30IsDone
-      }
-    }
-  }
-`;
-||||||| merged common ancestors
-=======
 export const searchChallengeByUser = /* GraphQL */ `
   query searchChallengeByUser($userID: String!) {
     listChallenges(filter: {userID: {eq: $userID}}) {
@@ -428,4 +351,3 @@ export const searchChallengeByUser = /* GraphQL */ `
     }
   }
 `;
->>>>>>> Stashed changes
