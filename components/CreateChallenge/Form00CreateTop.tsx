a2e4f2666407eb2daf039a84ea25c47e63887325
Travis Ricks <travis@travisricks.com>
2020-03-16 21:10:42 +0900

    start group challenges layout

---
+++ components/CreateChallenge/Form00CreateTop.tsx
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, TextInput, Alert} from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  H1,
+  H2,
+  H3,
+  Text,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+  DatePicker,
+} from 'native-base';
+import useStore from '../../state/state';
+
+function Form00CreateTop({navigation}, props) {
+  const state = useStore(state => state);
+
+  return (
+    <Container style={styles.Container}>
+      <Content>
+        <H1>You're only 30 days away from achieving a new goal</H1>
+        <Text>
+          You can create a personal individual challenge, create a group
+          challenge with your friends, or join an existing group challenge.
+        </Text>
+
+        <Button
+          style={styles.btn}
+          block
+          onPress={() => navigation.navigate('ChallengeType')}>
+          <Text>Make an Individual Challenge</Text>
+        </Button>
+        <Button
+          style={styles.btn}
+          block
+          onPress={() => navigation.navigate('ChallengeType')}>
+          <Text>Start a Group Challenge</Text>
+        </Button>
+        <Button
+          style={styles.btn}
+          block
+          onPress={() => navigation.navigate('ChallengeType')}>
+          <Text>Join a Group Challenge</Text>
+        </Button>
+      </Content>
+    </Container>
+  );
+}
+
+const styles = StyleSheet.create({
+  Container: {
+    padding: 20,
+    marginTop: 20,
+  },
+  Title: {
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  btn: {
+    marginTop: 20,
+  },
+});
+
+export default Form00CreateTop;
