097a4d8f1d7be597f737a7da26340385febeddda
KotaAoyama <hongcha811@gmail.com>
2020-03-16 10:07:01 +0900

    deleted unused state

--- state/state.js
+++ state/state.js
@@ -6,8 +6,6 @@ const [useStore] = create(set => ({
   setChallengeInput: value => set({challengeInput: value}),
   userCurrentChallenge: {},
   setUserCurrentChallenge: value => set({userCurrentChallenge: value}),
-  currentChallengeId: '0',
-  setCurrentChallengeId: value => set({currentChallengeId: value}),
   userHasActiveChallenge: false,
   setUserHasActiveChallenge: value => set({userHasActiveChallenge: value}),
   userFirstTime: false,
